4ed4c9edf59815441e36d85ab38dfbb42975ba28
UIofficialwebsite <uiwebsite638@gmail.com>
2025-10-22 02:16:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } "sonner";
+import { AuthProvider } from './hooks/useAuth.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+      <Toaster />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
