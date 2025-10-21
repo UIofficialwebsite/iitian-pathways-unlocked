b3680d575b7569d6d5608044d7e965ad7ecf7910
UIofficialwebsite <uiwebsite638@gmail.com>
2025-10-22 02:20:46 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Toaster } "sonner";
+import { Toaster } from "sonner";
 import { AuthProvider } from './hooks/useAuth.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
