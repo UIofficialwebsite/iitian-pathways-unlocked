e087be3324f5e22e95fb936bc59e2251702e982f
roshanyadav-2109 <businessmarketing0002@gmail.com>
2026-01-17 16:28:09 +0530

    Update CourseDetail.tsx

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { supabase } from '@/integrations/supabase/client';
 import { Course } from '@/components/admin/courses/types';
