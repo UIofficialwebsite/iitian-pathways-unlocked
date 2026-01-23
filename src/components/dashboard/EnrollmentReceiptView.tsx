98b7187b1d1ee5a46881c7792d4cc0e21871d1fc
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-01-23 23:15:22 +0530

    Update EnrollmentReceiptView.tsx

--- src/components/dashboard/EnrollmentReceiptView.tsx
+++ src/components/dashboard/EnrollmentReceiptView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import { useAuth } from '@/hooks/useAuth';
 import { supabase } from '@/integrations/supabase/client';
