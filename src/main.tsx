66abfde5c1255d2768bfe062918de0be0c2c6149
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-01-25 20:03:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+// Your Client ID
 const clientId = "29616950088-p64jd8affh5s0q1c3eq48fgfn9mu28e2.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
