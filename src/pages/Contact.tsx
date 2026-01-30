2f7b28a5b91dd89946fe2814e405996454ddf49e
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-01-30 17:30:12 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -106,14 +106,14 @@ const Contact = () => {
             {/* Right Section: Map Panel */}
             <div className="w-full lg:w-2/3 h-[450px] bg-gray-50 rounded-xl overflow-hidden border border-gray-200 shadow-sm">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83923192776!2d77.0688975472578!3d28.52728034389636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1701234567890!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83923192776!2d77.0688975472578!3d28.52728034389636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1709280000000!5m2!1sen!2sin" 
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }} 
                 allowFullScreen 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
-                title="Unknown IITians Location"
+                title="Unknown IITians Location - New Delhi"
                 className="filter grayscale-[20%] hover:grayscale-0 transition-all duration-500"
               ></iframe>
             </div>
